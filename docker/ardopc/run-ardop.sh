ac2c0367be5f2dabdf317bedf54d74bbb222d160
Joni Kähärä <joni.kahara@async.fi>
2022-10-16 19:45:49 +0000

    key from ardopc

--- docker/ardopc/run-ardop.sh
+++ docker/ardopc/run-ardop.sh
@@ -3,6 +3,6 @@
 gomplate < /etc/ardopc/asoundrc.tmpl > ${HOME}/.asoundrc
 
 while true; do
-      ardopc || true
+      ardopc --cat $RIG_SERIALPORT --keystring FEFE88E01C0001FD --unkeystring FEFE88E01C0000FD || true
       sleep 1
 done
--- docker/pat/run-pat.sh
+++ docker/pat/run-pat.sh
@@ -2,6 +2,6 @@
 
 gomplate < /etc/pat/config.json.tmpl > /etc/pat/config.json
 
-dockerize -wait tcp://rigctld:4532 -wait tcp://ardopc:8515
+dockerize -timeout 90s -wait tcp://rigctld:4532 -wait tcp://ardopc:8515
 
 pat --config /etc/pat/config.json http --addr 0.0.0.0:10000
