f0b68c7ac2e9a2f21483b0cd1fdba304f2c76216
Joni Kähärä <joni.kahara@async.fi>
2022-10-23 09:13:48 +0000

    tweak ardopc invocation

--- docker/ardopc/run-ardop.sh
+++ docker/ardopc/run-ardop.sh
@@ -3,6 +3,6 @@
 gomplate < /etc/ardopc/asoundrc.tmpl > ${HOME}/.asoundrc
 
 while true; do
-      ardopc --cat $RIG_SERIALPORT --keystring FEFE88E01C0001FD --unkeystring FEFE88E01C0000FD || true
+      ardopc 8515 pcm.ARDOP pcm.ARDOP || true  # --ptt ${RIG_SERIALPORT}:${RIG_SERIALSPEED} --keystring FEFE88E01C0001FD --unkeystring FEFE88E01C0000FD || true
       sleep 1
 done
